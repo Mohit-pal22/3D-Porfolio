1c0f1db987381ffc6ab5dcf6c9bbcce95b76aaf5
Mohit Pal <mohitpal2204@gmail.com>
2025-07-16 02:24:10 +0530

    Replaced react-tilt with react-parallax-tilt for React 18 compatibility

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,7 +8,14 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+  className="xs:w-[250px] w-full"
+  tiltMaxAngleX={25}
+  tiltMaxAngleY={25}
+  scale={1.05}
+  transitionSpeed={450}
+  glareEnable={false}
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
