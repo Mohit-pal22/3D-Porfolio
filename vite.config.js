53749e5ec5d67172f2d5750c268647918a69c824
Mohit Pal <mohitpal2204@gmail.com>
2025-07-16 02:08:34 +0530

    Configure base URL and add deploy scripts for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '3D-Portfolio-Live',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    // port: 3000,
+    cors: true,
+  }
 })
